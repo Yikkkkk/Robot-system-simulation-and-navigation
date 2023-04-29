72af88b460d40178fdfe1a5571395dd378451b31
yanglilin <2686333594@qq.com>
2023-04-30 01:11:06 +0800

    push fronts

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/yanglilin/ROS_demo/demo05_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/yanglilin/ROS_demo/demo05_ws/devel;/home/yanglilin/gazebo/devel;/opt/ros/noetic'.split(';') if '/home/yanglilin/ROS_demo/demo05_ws/devel;/home/yanglilin/gazebo/devel;/opt/ros/noetic' != '' else []
